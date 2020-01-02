7316423ec8e77854a3747efb0d5deff83ed8eb3e
Regulater27 <56885135+Regulater27@users.noreply.github.com>
2020-01-02 16:23:16 -0500

    Update main.py

--- app0/main.py
+++ app0/main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 def find_data():
     # Init headless ChromeDriver
-    chrome_path = r'/home/adam/fssaiscrape4/app0/chromedriver.exe'
+    chrome_path = r'/home/adam/fssaiscrape5/app0/chromedriver.exe'
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(executable_path = chrome_path, chrome_options=chrome_options)
